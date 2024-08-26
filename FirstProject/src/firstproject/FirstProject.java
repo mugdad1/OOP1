ac1ee6c89a56936a971e4e8fb8fa7708ab45d2db
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-08-26 15:46:21 +0300

    Update FirstProject.java

--- FirstProject/src/firstproject/FirstProject.java
+++ FirstProject/src/firstproject/FirstProject.java
@@ -4,12 +4,34 @@ package firstproject;
  *
  * @author mugdad
  */
-public class FirstProject {
+ class FirstProject {//class name and file.jaVA must be equal||i can remove the public in the begging 
 
     public static void main(String[] args) {
-        //main method follows main class
+//main method follows main class| java is case sensetive| string is a class |
         System.out.println("HELLO WORLD");
-        //جملة الطباعة تتبع كلاس السستمsystem.out
+        System.out.printf("%15s","fk"+"\n");
+//      جملة الطباعة تتبع كلاس السستمsystem.out
+
     }
 
 }
+/*
+Escape Sequences in Java:
+\n  - New line
+\t  - Tab
+\\  - Backslash
+\'  - Single quote
+\"  - Double quote
+\r  - Carriage return
+\b  - Backspace
+\f  - Form feed
+*/
+/*
+Main Format Specifiers for printf in Java:
+%d  - Decimal integer
+%f  - Floating-point number
+%s  - String
+%c  - Character
+%b  - Boolean
+%n  - New line (platform-independent)
+*/
