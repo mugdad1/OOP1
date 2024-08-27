1cb815b010b8a4e52362becfd1086e26c772251b
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-08-27 16:43:19 +0300

    .

--- FirstProject/src/firstproject/FirstProject.java
+++ FirstProject/src/firstproject/FirstProject.java
@@ -35,3 +35,23 @@ Main Format Specifiers for printf in Java:
 %b  - Boolean
 %n  - New line (platform-independent)
 */
+import java.util.Scanner; // program uses class Scanner
+public class Addition
+{
+// main method begins execution of Java application public static void main(String[] args)
+{
+// create a Scanner to obtain input from the command window Scanner input = new Scanner(System.in);
+int number1; // first number to add int number2; // second number to add
+
+
+int sum; // sum of number1 and number2
+
+Imports class Scanner for use in this program
+Creates Scanner for
+reading data from the
+user
+Variables that are declared but not initialized
+
+System.out.print("Enter first integer: "); // prompt number1 = input.nextInt(); // read first number from user
+System.out.print("Enter second integer: "); // prompt number2= input.nextInt(); // read second number from user
+sum = number1 + number2; // add numbers, then store total in sum
