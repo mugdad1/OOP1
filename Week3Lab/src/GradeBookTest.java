85e0392f42b74aa7244328ae58f245867c168720
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-09-08 16:53:16 +0300

    holyshit

--- Week3Lab/src/GradeBookTest.java
+++ Week3Lab/src/GradeBookTest.java
@@ -9,8 +9,6 @@ public class GradeBookTest {
 
         // Create a new Scanner object to read input from the keyboard
         Scanner input = new Scanner(System.in);
-        // Create a new GradeBook object with a default course name
-        GradeBook myGradeBook = new GradeBook("default");
 
         // Prompt the user to enter a course name
         System.out.println("Enter course name: ");
@@ -18,13 +16,37 @@ public class GradeBookTest {
         // Read the user's input and store it in a variable
         String nameofcourse = input.nextLine();
 
+        // Create a new GradeBook object with the user-entered course name
+        GradeBook myGradeBook = new GradeBook(nameofcourse);
+
         // Display a blank line
         System.out.println();
 
         // Call the displayMessage method with the provided course name
-        myGradeBook.displayMessage(nameofcourse);
+        myGradeBook.displayMessage();
+
+        // Call the setCourseName method to change the course name
+        myGradeBook.setCourseName("intro to java");
+
+        // Retrieve the updated course name
+        String courseName = myGradeBook.getCourseName();
+
+        // Print the updated course name to the console
+        System.out.println("The course name is: " + courseName);
+
+        // Attempt to call the non-existent x method
+        int z = myGradeBook.x();
+
+        // Print the result of the non-existent x method
+        System.out.println(z);
 
         // Close the Scanner object to release system resources
         input.close();
     }
 }
+/*
+ * consturcter is used to create an obj
+ * Each class can provide a constructor that initializes an object of a class when the object is created. 
+ * constructer must be same as a
+ * methods can return value but not constructer
+ */
