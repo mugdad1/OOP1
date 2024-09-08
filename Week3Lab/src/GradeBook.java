c98ebc98adf44c92c8fb91f4ac8bc6654164a719
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-09-08 14:12:32 +0300

    explains the code

--- Week3Lab/src/GradeBook.java
+++ Week3Lab/src/GradeBook.java
@@ -1,17 +1,24 @@
-
+// Define a class to represent a grade book for a school subject
 public class GradeBook {
 
+    // Declare a private field to store the name of the school subject
     private String courseName;
 
+    // Define a constructor to initialize the course name
     public GradeBook(String courseName) {
+        // Set the course name using the constructor parameter
         this.courseName = courseName;
     }
 
+    // Define a method to display a welcome message using the course name
     public void displayMessage() {
+        // Print a welcome message with the course name
         System.out.println("Welcome to the " + courseName + " GradeBook!");
     }
 
+    // Define an overloaded method to display a welcome message using a provided course name
     public void displayMessage(String courseName) {
+        // Print a welcome message with the provided course name
         System.out.println("Welcome to the " + courseName + " GradeBook!");
     }
 }
--- Week3Lab/src/GradeBookTest.java
+++ Week3Lab/src/GradeBookTest.java
@@ -1,15 +1,30 @@
 
+// Define a class to test the GradeBook class
 import java.util.Scanner;
 
 public class GradeBookTest {
 
+    // Define the main method to run the program
     public static void main(String[] args) {
+
+        // Create a new Scanner object to read input from the keyboard
         Scanner input = new Scanner(System.in);
+        // Create a new GradeBook object with a default course name
         GradeBook myGradeBook = new GradeBook("default");
+
+        // Prompt the user to enter a course name
         System.out.println("Enter course name: ");
+
+        // Read the user's input and store it in a variable
         String nameofcourse = input.nextLine();
+
+        // Display a blank line
         System.out.println();
+
+        // Call the displayMessage method with the provided course name
         myGradeBook.displayMessage(nameofcourse);
+
+        // Close the Scanner object to release system resources
+        input.close();
     }
 }
-//lol
