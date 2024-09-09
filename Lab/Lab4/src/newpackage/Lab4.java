d90fb4d1813d76fa399a1d811c1c6136865f3497
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-09-09 14:50:37 +0300

    Update Lab4.java

--- Lab/Lab4/src/newpackage/Lab4.java
+++ Lab/Lab4/src/newpackage/Lab4.java
@@ -5,10 +5,20 @@ package newpackage;
  * @author mugdad
  */
 class point {
-     point(){
-    x= 1;
-    y =2;
+//class
+
+    point() {
+// constructer 
+        x = 1;
+        y = 2;
     }
+    point(int _x, int _y){
+        x = _x;
+        y = _y;
+    }
+   
+     
+
 
     int x;
     int y;
@@ -18,9 +28,9 @@ class point {
 public class Lab4 {
 
     public static void main(String[] args) {
-        point p1 = new point();
-        System.out.println(p1.x);
-        System.out.println(p1.y);
+        point p1 = new point(5,5);
+        System.out.println(p1.x);//1
+        System.out.println(p1.y);//2
         p1.x = 10;
         p1.y = 20;
         System.out.println(p1.x);
