9fe3ee37aca972e8eb6a9978c2fdc86d26d360e9
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-09-09 15:25:59 +0300

    Update StudentsInformation.java

--- Lab/StudentsInformation/src/studentsinformation/StudentsInformation.java
+++ Lab/StudentsInformation/src/studentsinformation/StudentsInformation.java
@@ -10,11 +10,11 @@ public class StudentsInformation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Student st1 = new Student(101,"ahmed ", 4.5f );
-        Student st2 = new Student(101,"ahlam ", (float) 2.5 );
+        Student st1 = new Student(101, "ahmed ", 4.5f);
+        Student st2 = new Student(101, "ahlam ", (float) 2.5);
         st1.DisplayInfo();
         st2.DisplayInfo();
-        
+
     }
-    
+
 }
