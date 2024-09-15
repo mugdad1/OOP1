cf8a952423e1acf4fa3e6933de869ab46e0e2064
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-09-15 16:26:25 +0300

    assigenmnet2

---
+++ Theory/assignment1/src/main/java/mugdad1/assignment2.java
@@ -0,0 +1,18 @@
+package mugdad1;
+
+import java.util.Scanner;
+
+public class assignment2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the word you want to convert to captial");
+        String str = scanner.nextLine();
+        String uppercaseStr = convertToUppercase(str);
+        scanner.close();
+        System.out.println(uppercaseStr); // Output: "JJJ"
+    }
+
+    public static String convertToUppercase(String inputString) {
+        return inputString.toUpperCase();
+    }
+}
