0e8915ba4c3684c8aa06a68df580ceddfd6d6e7f
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-11-09 07:41:11 +0300

    .

--- Theory/ch8/src/main/java/mugdad1/Access.java
+++ Theory/ch8/src/main/java/mugdad1/Access.java
@@ -1,6 +1,6 @@
 package mugdad1;
 class Access {
-    static int x;
+     static int x;
 
     void increment() {
         x++;
@@ -8,14 +8,21 @@ class Access {
 }
 
 class StaticUse {
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Access obj1 = new Access();
         Access obj2 = new Access();
+        Access obj3 = new Access();
+
 
         obj1.x = 0; // Initialize x to 0
         obj1.increment(); // Increment x using obj1
         obj2.increment(); // Increment x using obj2
+        obj3.increment();
 
-        System.out.println(obj1.x + " " + obj2.x); // Print the value of x from both objects
+        System.out.println(obj1.x + " " + obj2.x + " " + obj3.x); // Print the value of x from both objects
     }
 }
